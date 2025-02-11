7a96b21f15199b0a819a6ddb82dcb85f1b2f3782
adham yehia <adhamyehia194@gmail.com>
2025-02-11 22:43:26 +0200

    Test

--- Assginment/Program.cs
+++ Assginment/Program.cs
@@ -1,10 +1,90 @@
-﻿namespace Assginment
+﻿
+
+namespace Assginment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Demo
+
+            #region Implicitly Typed Local Variable [Var , Dynamic]
+
+            #region Var
+
+            //// string Data = "Ahmed";
+
+            //var Data = "Ahmed"; // Implicit Type
+            //                    // Compiler WillDetect The Datatype Of Variable at Compilation Time
+            //                    // Based On its Intial Value
+
+            //// var X = null; // Invalid
+            //// Var Can't be Intialized With null
+
+            //Data = null; // Valid
+
+            //// Data = 5; // Invalid
+            //// After Intialization , You can't change the type of Var
+
+            //// var X; // Invalid
+            //// Var Must be Intialized
+
+            #endregion
+
+            #region Dynamic
+
+            //dynamic Data = "Adham";
+            //// Clr Will Detect Datatype Of Variable At Runtime
+            //// Based On it's last Assigned value
+
+            //dynamic X; // Valid
+            //// Can Declare Variable With Dynamic Without Intialization;
+
+            //dynamic Y = null; // Valid
+            //// Can Be Intialized With NUll
+
+            //Data = 2;
+
+            //// After intialization , You Can Change Datatype of Variable
+
+            //Data = true;
+            //Data = 3.3;
+            //Data = 'A';
+
+            #endregion
+
+            // Can't Use Var in Parameter Or Return Type Or Attribute at Class
+            // We Use It Only To Declare Local Variable
+
+            //for (var i = 0; i < 10; i++) { }
+            //foreach (var item in args) { }
+
+            // Var Is Recommended
+            // Errors => Compilation Time
+
+            // var X = null; // Compilation Error
+
+            //dynamic Y = null;
+            //Console.WriteLine(Y); //RuntimeBinderException
+
+            #endregion
+
+            #region Extension Methods
+
+            
+
+            int X = 12345;
+
+            
+            // int Y = ExtensionMethod.Reverse(X);
+
+            //int Y = X.Reverse();
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
---
+++ Demo/ExtensionMethod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class ExtensionMethod
+    {
+        // Extension Method
+        // Must be in Static and non Generic Class
+        //public static int Reverse( this int Number)
+        //{
+
+        //    int ReversedNumber = 0, Reminder;
+
+        //    while (Number != 0)
+        //    {
+        //        Reminder = Number % 10;
+        //        ReversedNumber = ReversedNumber * 10 + Reminder;
+        //        Number /= 10;
+        //    }
+
+        //    return ReversedNumber;
+        //}
+
+        //public static long Reverse(this long Number)
+        //{
+
+        //    long ReversedNumber = 0, Reminder;
+
+        //    while (Number != 0)
+        //    {
+        //        Reminder = Number % 10;
+        //        ReversedNumber = ReversedNumber * 10 + Reminder;
+        //        Number /= 10;
+        //    }
+
+        //    return ReversedNumber;
+        //}
+    }
+}
